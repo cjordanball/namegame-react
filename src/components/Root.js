9ce4fc125fd5a76d0fb5a94f8806f5967bb2b944
C. Jordan Ball III <cjordanball@ballwebdev.com>
2019-01-12 19:59:18 -0500

    initial setup of react project

---
+++ src/components/Root.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const Root = () => (
+	<Fragment>
+		<h1>This is a test!</h1>
+		<h3>This is a test!</h3>
+	</Fragment>
+)
+
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles/styles.less';
+import Root from './components/Root';
+
+ReactDOM.render(
+	<Root />, document.querySelector('#root')
+);
